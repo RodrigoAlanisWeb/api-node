c6203c54424cabae73068845744804849c6913c6
Rodrigo Alanis <rodri2007ro@gmail.com>
2021-01-17 16:14:20 -0600

    Final Version

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const fileupload = require('express-fileupload');
 const path = require('path');
 const User = require('../models/User');
 const verify_token = require('../verify_token');
 
 // Routes
-
-router.use(fileupload({
-    useTempFiles : true,
-    tempFileDir : path.join(__dirname,'tmp'),
-    debug: true
-}));
 
 router.put('/',verify_token,async (req,res)=>{
     const { username,name,email } = req.body;
@@ -44,35 +37,6 @@ router.put('/',verify_token,async (req,res)=>{
             res: false
         });
     }
-})
-
-router.post('/image',verify_token,async (req,res)=>{
-    const file = req.files.file;
-
-    file.mv(path.join(__dirname,'../uploads/'+file.name),(err)=>{
-        if (err) {
-            return res.status(500).send({
-                res: false,
-                msg: err
-            })
-        }      
-    });
-
-    const user = await User.findOneAndUpdate({_id:req.userId},{
-        image: file.name
-    });
-
-    if (user) {
-        return res.json({
-            res: true,
-        });
-    } else {
-        return res.status(500).send({
-            res: false
-        });
-    }
-
-
 })
 
 module.exports = router;
