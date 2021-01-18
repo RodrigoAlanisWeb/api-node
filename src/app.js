4b341cd89c6a150d6cb35204527de9a54bc9fc4d
Rodrigo Alanis <rodri2007ro@gmail.com>
2021-01-17 18:06:03 -0600

    Fixex 4

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization,x-access-token")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization,")
+    res.header("x-access-token")
     next();
 });
 
